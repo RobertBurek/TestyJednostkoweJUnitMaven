986a4d44fb782ac5fc30a1d2be0aa1b582f68d80
Robert Burek <robertburek@wp.pl>
2019-05-27 16:48:27 +0200

    Klasa licznika eletryczności wraz z pierwszymi testami - Assert

--- src/main/java/pl/robwork/App.java
+++ src/main/java/pl/robwork/App.java
@@ -1,13 +1,81 @@
 package pl.robwork;
 
+import java.util.Calendar;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
 
-        System.out.println( "Hello World!" );
+    private float kwhNoTariff = 0;
+    private int centsForKwh = 0;
+
+    private boolean tariffOn = false;
+    private float kwhTariff = 0;
+    private int centsForKwhTariff = 0;
+
+    private int electricityTariffStartHour = 0;
+    private int electricityTariffEndHour = 0;
+
+
+    public boolean isTariffNow() {
+        Calendar rightNow = Calendar.getInstance();
+        int hour = rightNow.get(Calendar.HOUR_OF_DAY);
+        return hour > electricityTariffStartHour && hour < electricityTariffEndHour;
+    };
+
+
+    public void addKwh(float kwhToAdd) {
+        if (isTariffNow()) {
+            kwhTariff += kwhToAdd;
+        } else {
+            kwhNoTariff += kwhToAdd;
+        }
+    }
+
+    /**
+     * @return how much more expensive is normal price comparing to tariff in percentages
+     * zwraca o ile jest droższy prąd normalnie w porównaniu do taryfy w procentach
+     */
+    public int getHowMoreExpensiveNormalIs() {
+        return (centsForKwh * 100 / centsForKwhTariff) - 100;
+    }
+
+    public float getKwhNoTariff() {
+        return kwhNoTariff;
+    }
+
+    public float getKwhTariff() {
+        return kwhTariff;
+    }
+
+    void setCentsForKwh(int centsForKwh) {
+        this.centsForKwh = centsForKwh;
+    }
+
+    void setTariffOn(boolean tariffOn) {
+        this.tariffOn = tariffOn;
+    }
+
+    void setCentsForKwhTariff(int centsForKwhTariff) {
+        this.centsForKwhTariff = centsForKwhTariff;
+    }
+
+    void setElectricityTariffStartHour(int electricityTariffStartHour) {
+        this.electricityTariffStartHour = electricityTariffStartHour;
+    }
+
+    void setElectricityTariffEndHour(int electricityTariffEndHour) {
+        this.electricityTariffEndHour = electricityTariffEndHour;
+    }
+
+    public void reset() {
+        this.kwhNoTariff = 0;
+        this.centsForKwh = 0;
+        this.tariffOn = false;
+        this.kwhTariff = 0;
+        this.centsForKwhTariff = 0;
+        this.electricityTariffStartHour = 0;
+        this.electricityTariffEndHour = 0;
     }
 }
--- src/test/java/pl/robwork/AppTest.java
+++ src/test/java/pl/robwork/AppTest.java
@@ -1,20 +1,78 @@
 package pl.robwork;
 
-import static org.junit.Assert.assertTrue;
-
+import com.sun.nio.sctp.MessageInfo;
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
+import static org.junit.Assert.*;
 
-        assertTrue( true );
-    }
+/**
+ * Created by Robert Burek
+ */
+public class AppTest {
+
+        @Test
+        public void addKwh_newMeter_properAddition () {
+            App electrisityMeter = new App();
+            electrisityMeter.addKwh(1);
+            Assert.assertTrue(electrisityMeter.getKwhNoTariff() == 1);
+        }
+
+        @Test
+        public void addKwh_newMeter2_properAddition () {
+            App electrisityMeter = new App();
+            electrisityMeter.addKwh(1);
+            electrisityMeter.addKwh(4);
+            Assert.assertTrue(electrisityMeter.getKwhNoTariff() == 5);
+        }
+
+        @Test
+        public void addKwh_newMeter5_properAddition () {
+            App electrisityMeter = new App();
+            electrisityMeter.addKwh(1);
+            electrisityMeter.addKwh(4);
+            electrisityMeter.addKwh(4);
+            electrisityMeter.addKwh(3);
+            electrisityMeter.addKwh(2);
+            Assert.assertTrue(electrisityMeter.getKwhNoTariff() == 14);
+        }
+
+        @Test
+        public void addKwh_newMeter12_properAddition () {
+            App electrisityMeter = new App();
+            for (int i = 1; i <= 12; i++)
+                electrisityMeter.addKwh(i);
+            Assert.assertTrue(electrisityMeter.getKwhNoTariff() == 78);
+        }
+
+        @Test
+        public void kwhCounterIncreaseIfNew () {
+            App electrisityMeter = new App();
+            electrisityMeter.addKwh(5);
+            Assert.assertTrue(electrisityMeter.getKwhNoTariff() == 5);
+        }
+
+        @Test
+        public void kwhCounterIncreaseIfSecond () {
+            App electrisityMeter = new App();
+            electrisityMeter.addKwh(5);
+            electrisityMeter.addKwh(4);
+            Assert.assertTrue(electrisityMeter.getKwhNoTariff() == 9);
+        }
+
+        @Test
+        public void givenNewMeterWhenFirstAdditionThenProperCounter () {
+            App electrisityMeter = new App();
+            electrisityMeter.addKwh(5);
+            Assert.assertTrue(electrisityMeter.getKwhNoTariff() == 5);
+        }
+
+        @Test
+        public void givenNewMeterWhenFirstAdditionThenProperCounterTwoAssert () {
+            App electrisityMeter = new App();
+            electrisityMeter.addKwh(5);
+                Assert.assertTrue("Dodano 5 kwh a czekiwano 7 kwh", electrisityMeter.getKwhNoTariff() == 7);
+                // gdy pierwsza nie przejdzie nie mamy informacji o drugiej
+            Assert.assertTrue("Dodano 5 kwh i oczekiwano 5 kwh",electrisityMeter.getKwhNoTariff() == 5);
+        }
 }
